da84a13eba387bdb1344be8dc838d9f738175749
thonguyen14 <80441573+thonguyen14@users.noreply.github.com>
2022-11-03 17:50:54 +0700

    Update vortex_limit.sh

--- task/ACT-4/vortex_limit.sh
+++ task/ACT-4/vortex_limit.sh
@@ -12,7 +12,7 @@ ACC=$(seid q account $SEI_ADDR -o json --node https://sei-testnet-rpc.polkachu.c
 seq=$(seid q account $SEI_ADDR -o json --node https://sei-testnet-rpc.polkachu.com:443 | jq -r .sequence); sleep 7;
 
 #seid tx sign $HOME/SEI_ACT4/gen_limit_tx.json -s $seq -a $ACC --offline --from $wallet --chain-id atlantic-1 --output-document $HOME/SEI_ACT4/limit_tx.json --node https://sei-testnet-rpc.polkachu.com:443; sleep 7;
-seid tx sign $HOME/SEI_ACT4/gen_limit_tx.json -s $seq -a $ACC --offline --from $wallet --chain-id atlantic-1 --output-document $HOME/SEI_ACT4/limit_tx.json --node https://sei-testnet-rpc.polkachu.com:443; sleep 10;
+echo abcdef | seid tx sign $HOME/SEI_ACT4/gen_limit_tx.json -s $seq -a $ACC --offline --from $wallet --chain-id atlantic-1 --output-document $HOME/SEI_ACT4/limit_tx.json --node https://sei-testnet-rpc.polkachu.com:443; sleep 10;
 
 #date -R >> $HOME/SEI_ACT4/txh.log
 echo "Limit order:" >> $HOME/SEI_ACT4/txh.log
